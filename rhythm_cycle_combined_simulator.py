f683c66944262461239aa071f4c7273709c87ae0
James Chu <jameschu809@gmail.com>
2016-01-14 20:53:31 -0500

    Create rhythm_cycle_combined_simulator.py

---
+++ rhythm_cycle_combined_simulator.py
@@ -0,0 +1,114 @@
+# Expends the previous rhythm cycle patch. 
+# Adds a combined cycle feature
+# that reveals the composite rhythm.
+
+import sys
+
+
+def cycle(count, bar_length, length_string=104):
+	"""
+	Returns a string representation of a single cycle.
+	'X' denotes a rhythmic hit, '-'' indicates a rest.'|'' 
+	represents the start of a new bar.
+	"""
+
+	cycle = ''
+
+	for i in range(length_string):
+
+		if i % bar_length == 0:
+
+			cycle += '|'
+
+		if i % count == 0:
+
+			cycle += 'X'
+
+		else:
+
+			cycle += '-'
+
+
+	return cycle
+
+
+def combine_cycles(lists_of_cycles):
+	"""
+	Takes in a list of list of all cycles and returns a 
+	cycle indicated any instance when there was a hit
+	amongst the cycles.
+	"""
+
+	combined_timeline = ['-' for i in range(len(lists_of_cycles[0]))]
+
+
+	for cycle in lists_of_cycles:
+
+		for i in range(len(cycle)):
+
+			if cycle[i] == '|':
+
+				combined_timeline[i] = '|'
+
+			elif cycle[i] == 'X':
+
+				combined_timeline[i] = 'X'
+
+
+	combined = ''
+
+
+	for i in combined_timeline:
+
+
+		combined += i
+
+
+	print 'Combined cycle:'
+
+
+	print combined
+
+
+
+def main():
+
+	cycles = []
+
+	for i in range(num_concurrent_cycles):
+
+
+		if i == 0: # Get ride of first sys argument.
+
+
+			pass
+
+
+		else:
+
+
+			print cycle(int(sys.argv[i]), bar_length)
+
+
+			cycles.append(list(cycle(int(sys.argv[i]), bar_length)))
+
+
+
+	combine_cycles(cycles)
+
+
+if __name__ == '__main__':
+
+
+	if len(sys.argv) < 2:
+
+
+		print 'Enter, counts per rhythmic cycle (as many as you want), barring structure.'
+
+
+	else:
+
+		num_concurrent_cycles = len(sys.argv) - 1
+		bar_length = int(sys.argv[-1])
+
+		main()
